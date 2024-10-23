2a4521ba6863f13f2ad5712e9535c12391e2ba9d
98.danya <1998.danyamohammed@gmail.com>
2024-10-23 23:59:40 +0200

    Updated the UI and included a reset button

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 const container = document.querySelector("#container");
 const btn = document.querySelector("#button");
+const reset = document.querySelector("#reset");
 
 function createGrid(size) {
     container.innerHTML= '';
     document.documentElement.style.setProperty('--grid-size', size);
 
-
     for (i = 0; i < (size * size); i++) {
         const square = document.createElement("div");
         container.appendChild(square);
         square.addEventListener("mouseover", () => {
-            //square.style.backgroundColor = "red";
             square.style.backgroundColor = "hsl(" + 360 * Math.random() + ', 90%, 80%)';
 
         })
@@ -28,5 +27,8 @@ createGrid(16);
             alert("Try again! Only a value between 0 and 100!")
         }
     });
-   
-    
+
+    reset.addEventListener('click', (event) => {
+        container.innerHTML = ""
+        createGrid(16)
+      });
